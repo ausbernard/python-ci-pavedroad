0f99ae512d84ebef646309e4b2c01d77a26f60f9
ausbernard <solegrumpy@gmail.com>
2025-11-02 21:25:55 -0500

    remove git operations

--- assets/hook-loop.sh
+++ assets/hook-loop.sh
@@ -3,5 +3,3 @@
 pre-commit run --all-files   # see failures
 ruff check . --fix           # auto-fix most issues
 black .                      # format
-git add -A                   # re-stage fixed files
-git commit                   # try again
